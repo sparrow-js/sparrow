754f66364ef16e9de3f8fcd0d93f15b7d1c05390
Haitao Wu <wuhaitao@xiaogetech.com>
2019-12-10 10:54:37 +0800

    fix

---
+++ market/blocks/example/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': ['warn']
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
---
+++ market/blocks/example/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <basic-form></basic-form>
+  </div>
+</template>
+
+<script>
+import BasicForm from '../../../BasicForm/src';
+export default {
+  name: "home",
+  components: {
+    BasicForm
+  }
+};
+</script>
