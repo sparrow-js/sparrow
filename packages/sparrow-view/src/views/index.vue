5b0bcda18322f07f3f6345d58515f14cf0dfda7b
Haitao Wu <sparrowwht7@gmail.com>
2020-01-31 15:18:36 +0800

    no message

--- packages/sparrow-view/src/views/index.vue
+++ packages/sparrow-view/src/views/index.vue
@@ -2,18 +2,6 @@
   <div class="home">
     <div class="block-list">
       <box :index="0" class="block-item">
-        <block><BasicForm3 /></block>
-      </box>
-    </div>
-
-    <div class="block-list">
-      <box :index="1" class="block-item">
-        <block />
-      </box>
-    </div>
-
-    <div class="block-list">
-      <box :index="2" class="block-item">
         <paragraph />
       </box>
     </div>
@@ -21,12 +9,8 @@
 </template>
 
 <script>
-import BasicForm3 from "./components/BasicForm3";
 import generatorMixin from "../mixins/generatorMixin";
 export default {
-  components: {
-    BasicForm3
-  },
   mixins: [generatorMixin]
 };
 </script>
