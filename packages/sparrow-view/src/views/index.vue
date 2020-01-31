196a6ce72a963689242dc090b28a9f11a1750c32
Haitao Wu <sparrowwht7@gmail.com>
2020-01-31 13:18:22 +0800

    fix

--- packages/sparrow-view/src/views/index.vue
+++ packages/sparrow-view/src/views/index.vue
@@ -2,13 +2,23 @@
   <div class="home">
     <div class="block-list">
       <box :index="0" class="block-item">
-        <block><BasicTable1 /></block>
+        <block><SelectTable /></block>
       </box>
     </div>
 
     <div class="block-list">
       <box :index="1" class="block-item">
-        <block><BasicForm /></block>
+        <el-form label-width="100px">
+          <layout :col="3" :row="2">
+            <el-form-item label="基础文本框" prop="name" slot="0_0">
+              <el-input />
+            </el-form-item>
+
+            <el-form-item label="基础文本框" prop="name" slot="0_2">
+              <el-input />
+            </el-form-item>
+          </layout>
+        </el-form>
       </box>
     </div>
 
@@ -21,13 +31,11 @@
 </template>
 
 <script>
-import BasicForm from "./components/BasicForm";
-import BasicTable1 from "./components/BasicTable1";
+import SelectTable from "./components/SelectTable";
 import generatorMixin from "../mixins/generatorMixin";
 export default {
   components: {
-    BasicTable1,
-    BasicForm
+    SelectTable
   },
   mixins: [generatorMixin]
 };
