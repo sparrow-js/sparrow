f0363ad72c3befc2d82c6541b468714e920cd000
Haitao Wu <sparrowwht7@gmail.com>
2020-02-14 20:29:02 +0800

    fix

--- packages/sparrow-view/src/views/index.vue
+++ packages/sparrow-view/src/views/index.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <div class="block-list">
       <box :index="0" class="block-item">
+        <block />
+      </box>
+    </div>
+
+    <div class="block-list">
+      <box :index="1" class="block-item">
         <paragraph />
       </box>
     </div>
