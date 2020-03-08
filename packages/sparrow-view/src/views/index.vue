a8b72011b9837ce438760e1290f508d2d7636d9f
Haitao Wu <sparrowwht7@gmail.com>
2020-03-08 20:44:27 +0800

    fix

--- packages/sparrow-view/src/views/index.vue
+++ packages/sparrow-view/src/views/index.vue
@@ -2,18 +2,6 @@
   <div class="home">
     <div class="block-list">
       <box :index="0" class="block-item">
-        <BasicForm />
-      </box>
-    </div>
-
-    <div class="block-list">
-      <box :index="1" class="block-item">
-        <SearchBox />
-      </box>
-    </div>
-
-    <div class="block-list">
-      <box :index="2" class="block-item">
         <paragraph />
       </box>
     </div>
@@ -21,14 +9,8 @@
 </template>
 
 <script>
-import SearchBox from "./components/SearchBox";
-import BasicForm from "./components/BasicForm";
 import generatorMixin from "../mixins/generatorMixin";
 export default {
-  components: {
-    BasicForm,
-    SearchBox
-  },
   mixins: [generatorMixin]
 };
 </script>
