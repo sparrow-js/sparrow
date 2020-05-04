762f7cea79e0ff7c82429545fc462ff117c3ba1f
Haitao Wu <sparrowwht7@gmail.com>
2020-05-04 12:20:15 +0800

    fix: 修改依赖版本

--- packages/sparrow-view/src/views/components/Table1.vue
+++ packages/sparrow-view/src/views/components/Table1.vue
@@ -4,65 +4,41 @@
       <el-table border="" style="width: 100%" :data="tableData">
         <el-table-column label="">
           <template slot="header" slot-scope="{ row, column, $index }">
-            <table-header-box uuid="d3a87972" :label="column.label" />
+            <table-header-box uuid="05da923c" :label="column.label" />
           </template>
 
           <template slot-scope="{ row, column, $index }">
-            <table-cell-box uuid="d3a87972" />
+            <table-cell-box uuid="05da923c" />
           </template>
         </el-table-column>
 
         <el-table-column label="">
           <template slot="header" slot-scope="{ row, column, $index }">
-            <table-header-box uuid="a879722c" :label="column.label" />
+            <table-header-box uuid="da923c91" :label="column.label" />
           </template>
 
           <template slot-scope="{ row, column, $index }">
-            <table-cell-box uuid="a879722c" />
+            <table-cell-box uuid="da923c91" />
           </template>
         </el-table-column>
 
         <el-table-column label="">
           <template slot="header" slot-scope="{ row, column, $index }">
-            <table-header-box uuid="79722c4a" :label="column.label" />
+            <table-header-box uuid="923c91ec" :label="column.label" />
           </template>
 
           <template slot-scope="{ row, column, $index }">
-            <el-tag :type="'info'" size="small">
-              北京
-            </el-tag>
-
-            <el-tag :type="'success'" size="small">
-              测试
-            </el-tag>
-
-            <table-cell-box uuid="79722c4a" />
+            <table-cell-box uuid="923c91ec" />
           </template>
         </el-table-column>
 
         <el-table-column label="">
           <template slot="header" slot-scope="{ row, column, $index }">
-            <table-header-box uuid="722c4ab7" :label="column.label" />
+            <table-header-box uuid="3c91ec8a" :label="column.label" />
           </template>
 
           <template slot-scope="{ row, column, $index }">
-            <el-button
-              size="mini"
-              type="success"
-              @click="handleEdit(row, $index)"
-            >
-              编辑
-            </el-button>
-
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(row, $index)"
-            >
-              删除
-            </el-button>
-
-            <table-cell-box uuid="722c4ab7" />
+            <table-cell-box uuid="3c91ec8a" />
           </template>
         </el-table-column>
       </el-table>
@@ -72,26 +48,6 @@
 
 <script>
 export default {
-  methods: {
-    handleDelete(row, index) {
-      this.$notify({
-        title: "Success",
-        message: "Delete Successfully",
-        type: "success",
-        duration: 2000
-      });
-    },
-
-    handleEdit(row, index) {
-      this.$notify({
-        title: "Success",
-        message: "Delete Successfully",
-        type: "success",
-        duration: 2000
-      });
-    }
-  },
-
   data() {
     return {
       tableData: [{}, {}, {}]
