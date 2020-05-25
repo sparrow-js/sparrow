e61984bb8c1d103f770814ddcbc757f746de4e7c
Haitao Wu <sparrowwht7@gmail.com>
2020-05-25 19:09:25 +0800

    fix

--- packages/sparrow-view/src/views/index.vue
+++ packages/sparrow-view/src/views/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="home">
     <div class="block-list">
-      <box :index="0" :label="'表单'" class="block-item">
-        <Basic />
-      </box>
-    </div>
-
-    <div class="block-list">
-      <box :index="1" class="block-item">
+      <box :index="0" class="block-item">
         <paragraph />
       </box>
     </div>
@@ -15,12 +9,8 @@
 </template>
 
 <script>
-import Basic from "./components/Basic";
 import generatorMixin from "../mixins/generatorMixin";
 export default {
-  components: {
-    Basic
-  },
   mixins: [generatorMixin]
 };
 </script>
