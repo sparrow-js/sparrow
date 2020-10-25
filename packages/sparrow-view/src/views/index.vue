b7cd24c1ca7003ac3ff20b475cccb3ac5dc2a027
Haitao Wu <sparrowwht7@gmail.com>
2020-10-25 14:15:18 +0800

    dco：修改文档描述

--- packages/sparrow-view/src/views/index.vue
+++ packages/sparrow-view/src/views/index.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="home drag-box" data-id="088d3d6d">
+  <div class="home drag-box" data-id="0964baa0">
     <img
       style="width: 400px;height: 400px;position: absolute;left: 50%;transform: translate(-200px, 100px);"
       src="https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/dded9db02e3f4052bbf451f04d3d9b5b~tplv-k3u1fbpfcp-zoom-1.image"
     />
-    <div />
   </div>
 </template>
 
