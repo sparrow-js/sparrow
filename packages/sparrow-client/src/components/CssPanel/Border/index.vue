76c8c742b724b34445f052fe3dbfefb3ce283f11
Haitao Wu <sparrowwht7@gmail.com>
2021-02-07 16:44:10 +0800

    fix

--- packages/sparrow-client/src/components/CssPanel/Border/index.vue
+++ packages/sparrow-client/src/components/CssPanel/Border/index.vue
@@ -1,11 +1,47 @@
 <template>
-  <div></div>
+  <div>
+    <css-item label="圆角">
+      <s-radio-group :list="borderRadiuslist" />
+    </css-item>
+    <css-item>
+      <el-slider v-model="value" show-input> </el-slider>
+    </css-item>
+  </div>
 </template>
 <script>
-export default {
-  
-}
-</script>
-<style lang="scss" scoped>
+import SRadioGroup from '../RadioGroup';
+import CssItem from '../CssItem';
 
-</style>
+export default {
+  components: {
+    SRadioGroup,
+    CssItem
+  },
+  data() {
+    return {
+      borderForm: {
+        border: '',
+        borderTopLeftRadius: '',
+        borderTopRightRadius: '',
+        borderBottomLeftRadius: '',
+        borderBottomRightRadius: '',
+        borderRadius: '',
+        boxShadow: ''
+      },
+      borderRadiuslist: [
+        {
+          label: '固定圆角',
+          icon: '',
+          value: ''
+        },
+        {
+          label: '独立定义',
+          icon: '',
+          value: ''
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ packages/sparrow-client/src/components/CssPanel/BorderModel/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="12">
+        <div></div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          <el-input-number
+            v-model="num"
+            controls-position="right"
+            @change="handleChange"
+          ></el-input-number>
+        </div>
+        <div>
+          <el-input placeholder="请输入内容" v-model="input2">
+            <template slot="append">
+              <el-popover placement="bottom" width="225" trigger="click">
+                <chrome-picker
+                  v-model="backgroundForm.backgroundColor"
+                ></chrome-picker>
+                <span slot="reference">color</span>
+              </el-popover>
+            </template>
+          </el-input>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+import { Chrome } from 'vue-color';
+
+export default {
+  components: {
+    ChromePicker: Chrome
+  }
+};
+</script>
+<style lang="scss" scoped></style>
